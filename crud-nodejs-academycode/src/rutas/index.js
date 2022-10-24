a4510a965e488d83dba1f79b088fb62b92ea1ba0
DavidgonzalezRuiz <113668358+DavidgonzalezRuiz@users.noreply.github.com>
2022-10-24 01:08:37 -0500

    metodos PUT y DELETE
    creación de los métodos para el consumo de la API rest

--- crud-nodejs-academycode/src/rutas/index.js
+++ crud-nodejs-academycode/src/rutas/index.js
@@ -1,13 +1,14 @@
 const { Router } = require("express")
 const router = Router()
 const cursos = require('../pro_data.json')
-
+//LEER
 router.get('/cursos', (req , res) => {
     res.json(cursos)  //el servidor envia un obj json
 })
+//crear
 router.post('/cursos/crear', (req , res) => {
    const {id,curso,precio,profesor,duracion}= req.body
-   if ( precio && profesor && duracion){       
+   if (id){       
        const newproduct = {...req.body, id}
        console.log(newproduct);
        cursos.push(newproduct)
@@ -16,5 +17,23 @@ router.post('/cursos/crear', (req , res) => {
    res.json('Peticion erronea')
    }
 })
-
-module.exports = router
+//actulizarr
+router.put('/cursos/actualizar/:id',(req,res)=>{
+    const cursoAcutualizado = req.body;
+    const id = req.params.id
+    const indice = cursos.findIndex(cursos => cursos.id === id);
+if(indice>= 0){
+    cursos[indice] = cursoAcutualizado;
+}
+res.json(cursos)
+})
+//eliminar
+router.delete('/cursos/eliminar/:id',(req,res)=>{
+    const id =req.params.id
+    const indice = cursos.findIndex(cursos => cursos.id == id);
+    
+        cursos.splice(indice, 1);
+        
+        res.json(cursos)
+})
+module.exports = route
